e6a3f76772da8d5be2ccceef950fa6b2cb716113
Martin Poulsen <mapo0004@stud.kea.dk>
2025-01-31 10:34:21 +0100

    added the variables file

---
+++ 01._Miscellaneous/variables.js
@@ -0,0 +1,61 @@
+// avoide using var, use let or const instead
+// var name = 'Martin';
+// var finder ikke fejl med samme variabelnavn
+
+const name = 'Martin';
+
+// const is NOT a constant
+// that means that const CAN have its value changed
+// cosnst is a constant in the DECLARATION
+// const MUST be delcared as it is initialized
+
+// name = "Martin";
+
+// ændre name værdien
+// objecter og arrays
+const me = {
+    name: 'Martin'
+};
+me.name = "test";
+
+console.log(me);
+
+// making a list with code
+const hobbies = ['test','coding'];
+
+// removing last added ('coding')
+hobbies.pop();
+
+// adding 'drawing' and 'painting'
+hobbies.push('drawing', 'painting');
+
+console.log(hobbies);
+
+const jsObject = {
+    name: 'Martin',
+    // comment
+};
+
+// "" vs. ''
+
+const dancerName = "Computer"
+
+// "" or '' or ´´ = string literals
+console.log("I can compile!");
+console.log('I can compile!');
+// ´ (backticks)
+// ´´ = template strings
+// ${} synstax = string interpolation
+console.log(`${dancerName}  can dance '' ""
+    line 1
+    line 2
+    ""''""`);
+
+// console.log m vs . +
+
+const assignmentDescription = "...and the value is...";
+const value = 4;
+
+// don't use plus in the console.log to avoid type coercion
+// + bliver 4 tallet hvidt da den bliver til en string
+console.log(assignmentDescription, value);
