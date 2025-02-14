4c468e21941bcd6ec854a4244381e2ffc812e3d5
Martin Poulsen <mapo0004@stud.kea.dk>
2025-02-14 09:45:52 +0100

    add
    add

--- 02._First_Server/app.js
+++ 02._First_Server/app.js
@@ -1,3 +1,4 @@
+// importere med require
 const express = require('express');
 const app = express();
 
---
+++ 03_Finter_Bones_Rest_API/app.js
@@ -0,0 +1,11 @@
+// importere med require
+const express = require("express");
+
+// vil gerne instansiere express
+const app = express();
+
+
+
+// skal lytte til en port
+const PORT = 8080;
+app.listen(PORT);
